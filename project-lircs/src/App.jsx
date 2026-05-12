a00b27acb71c5ffc0c96e76c86086f61442539b7
beerus <ajdbriones01202401029@usep.edu.ph>
2026-05-13 00:18:24 +0800

    init

---
+++ project-lircs/src/App.jsx
@@ -0,0 +1,8 @@
+import HeaderLogEntry from "./HeaderLogEntry.jsx";
+
+function App() {
+    return (
+        <HeaderLogEntry></HeaderLogEntry>
+    );
+}
+export default App
---
+++ project-lircs/src/HeaderLogEntry.jsx
@@ -0,0 +1,17 @@
+function HeaderLogEntry() {
+    return (
+        <header className="header-log-entry">
+            <h1>Log Entry</h1>
+
+            <nav id="nav-log-entry">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Search Books</a></li>
+                    <li><a href="#">My Status</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default HeaderLogEntry;
